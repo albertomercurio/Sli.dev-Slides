177b9b8c8fe86b46eafdc44428b2222b5badaa86
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-06-11 03:24:12 +0200

    Working initial example

--- 2025/usc-ssc/vite.config.js
+++ 2025/usc-ssc/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import Markdown from 'unplugin-vue-markdown/vite'
+import MarkdownItPrism from 'markdown-it-prism'
+import mdKatex from 'markdown-it-katex'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({ include: [/\.vue$/, /\.md$/] }),
+    Unocss(),
+    Markdown({
+      // same hooks, but names are now camel-cased like VitePress
+      markdownItOptions: { html: true, linkify: true, typographer: true },
+      markdownItSetup(md) {            // ✔ identical to Slidev/VitePress
+        md.use(MarkdownItPrism)
+        md.use(mdKatex)
+      },
+      headEnabled: true,               // expose front-matter to <head>
+      wrapperClasses: 'prose m-auto',  // optional
+    }),
+  ]
 })
